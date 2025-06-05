b13349303ca8707a18fd064c3fe4783b2799b659
Xehros <xehroscyber@gmail.com>
2025-06-05 21:04:24 +0200

    correct error mobile

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,30 @@ gsap.registerPlugin(ScrollTrigger);
 
 const sections = document.querySelectorAll("section");
 
-sections.forEach((section) => {
-  const id = section.id;
+// Usamos matchMedia para habilitar las animaciones en todos los tamaños de pantalla
+ScrollTrigger.matchMedia({
+  // Para todas las pantallas
+  "(min-width: 0px)": function() {
+    sections.forEach((section) => {
+      const id = section.id;
 
-  gsap.fromTo(section,
-    { opacity: 0, scale: 0.5 },
-    {
-      opacity: 1,
-      scale: 1,
-      ease: "power2.inOut",
-      duration: 5,
-      scrollTrigger: {
-        trigger: section,
-        start: "top 90%",
-        end: "top 50%",
-        scrub: 1
-      }
-    }
-  );
+      gsap.fromTo(section,
+        { opacity: 0, scale: 0.5 },
+        {
+          opacity: 1,
+          scale: 1,
+          ease: "power2.inOut",
+          duration: 2,
+          scrollTrigger: {
+            trigger: section,
+            start: "top 90%",
+            end: "top 50%",
+            scrub: 1
+          }
+        }
+      );
+    });
+  }
 });
 
 // Interceptar los clics del menú y controlar el scroll
@@ -29,18 +35,16 @@ document.querySelectorAll('nav a').forEach(link => {
     const targetId = link.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetId);
     if (targetSection) {
-        targetSection.scrollIntoView({ behavior: 'smooth', block: 'center' });
-
-        // Resetear animación si la necesitas
-        gsap.set(targetSection, { clearProps: "all" });
+      targetSection.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      gsap.set(targetSection, { clearProps: "all" });
     }
 
-    // Cerrar el menú de navegación si está abierto
     const navbarToggler = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('.navbar-collapse');
     if (navbarToggler && navbarCollapse.classList.contains('show')) {
-      navbarToggler.click(); // simula el clic para cerrar el menú
+      navbarToggler.click();
     }
   });
 });
+
 
--- scripts/menu.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.body.classList.add("menu-open");
-}
-
-function closeMenu() {
-    document.getElementById("sidebar").style.width = "0";
-    document.body.classList.remove("menu-open");
-}
